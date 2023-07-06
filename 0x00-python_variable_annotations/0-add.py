3d49a49df6340ecb65a506dc62bdf703cd5533cf
Ablene1 <111275457+Ablene1@users.noreply.github.com>
2023-07-06 19:22:14 +0900

    a

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-"""Type-annotated function add"""
+"""
+    Module with typing Python
+"""
 
 
 def add(a: float, b: float) -> float:
-    """Takes two floats and returns their sum as float"""
-    return a + b
+    """
+        Args:
+            a: The first parameter
+            b: The second parameter
+
+        Return:
+            Sum of two floats
+
+    """
+
+    return (a + b)
